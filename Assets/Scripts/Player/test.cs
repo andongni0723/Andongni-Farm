83972d6725335753b024c3c4783e5e4a938da86c
andongni <x102039@gmail.com>
2022-03-13 16:55:05 +0800

    SP.15 00:00

---
+++ Assets/Scripts/Player/test.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor.UI;
+
+
+public class test : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    public float x = 0;
+    private void Awake()
+    {
+        
+    }
+}

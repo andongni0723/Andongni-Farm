129b97080ae62965affbe6d819f14ee1c727ebb0
andongni <x102039@gmail.com>
2022-03-28 20:58:48 +0800

    SP.37 00:00

---
+++ Assets/Scripts/Time/Logic/TimeManager.cs
@@ -0,0 +1,99 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimeManager : MonoBehaviour
+{
+    private int gameSecond, gameMinute, gameHour, gameDay, gameMonth, gameYear;
+    private Season gameSeason = Season.春天;
+    private int monthInSeason = 3;
+
+    public bool gameClockPause;
+    private float tikTime;
+
+    private void Awake()
+    {
+        NewGameTime();
+    }
+
+    private void Update()
+    {
+        if (!gameClockPause)
+        {
+            tikTime += Time.deltaTime;
+
+            if (tikTime >= Settings.secondThreshold)
+            {
+                tikTime -= Settings.secondThreshold;
+
+                UpdateGameTime();
+            }
+        }
+    }
+
+    private void NewGameTime()
+    {
+        gameSecond = 0;
+        gameMinute = 0;
+        gameHour = 7;
+        gameDay = 1;
+        gameMonth = 1;
+        gameYear = 2022;
+        gameSeason = Season.春天;
+    }
+
+    private void UpdateGameTime()
+    {
+        gameSecond++;
+        if (gameSecond > Settings.secondHold)
+        {
+            gameMinute++;
+            gameSecond = 0;
+
+            if (gameMinute > Settings.minuteHold)
+            {
+                gameHour++;
+                gameMinute = 0;
+
+                if (gameHour > Settings.dayHold)
+                {
+                    gameDay++;
+                    gameHour = 0;
+
+                    if (gameDay > Settings.dayHold)
+                    {
+                        gameMonth++;
+                        gameDay = 1;
+
+                        if (gameMonth > 12)
+                            gameMonth = 1;
+
+                        monthInSeason--;
+                        if (monthInSeason == 0)
+                        {
+                            monthInSeason = 3;
+
+                            int seasonNum = (int)gameSeason;
+                            seasonNum++;
+
+                            if (seasonNum > Settings.seasonHold)
+                            {
+                                seasonNum = 0;
+                                gameYear++;
+                            }
+
+                            gameSeason = (Season)seasonNum;
+
+                            if (gameYear > 9999)
+                            {
+                                gameYear = 2022;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        Debug.Log("Second: " + gameSecond + " Minute: " + gameMinute);
+    }
+}
--- Assets/Scripts/Utilities/Settings.cs
+++ Assets/Scripts/Utilities/Settings.cs
@@ -6,4 +6,12 @@ public class Settings
 {
     public const float fadeDuration = 0.35f;
     public const float targetAlpha = 0.45f;
+
+    // Time
+    public const float secondThreshold = 0.01f; // Value smaller, Game Time faster
+    public const int secondHold = 59;
+    public const int minuteHold = 59;
+    public const int hourHold = 23;
+    public const int dayHold = 10;
+    public const int seasonHold = 3;
 }
